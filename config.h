75d1a2508cb867ab6a82de7daf00464adf0b4b58
aa-83 <elhegre@protonmail.com>
2020-07-22 17:21:53 +0200

    Updates

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true", "adobe-source-han-serif-otc-fonts=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=12";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -72,7 +72,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x30", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x26", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -91,20 +91,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,			    XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_j,      incrgaps,       {.i = +1 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_k,      incrgaps,       {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_m,      togglegaps,     {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_n,      defaultgaps,    {0} },
-/*	{ MODKEY|ControlMask|ShiftMask, XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY,                       XK_m,      togglegaps,     {0} },
+	{ MODKEY,                       XK_n,      defaultgaps,    {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_h,      incrogaps,      {.i = +1 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_l,      incrogaps,      {.i = -1 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_j,      incrigaps,      {.i = +1 } },
 	{ MODKEY|ControlMask|ShiftMask, XK_k,      incrigaps,      {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_y,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_o,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_y,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_o,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_y,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_o,      incrovgaps,     {.i = -1 } },*/
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
@@ -113,10 +105,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[5]} },
-	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[5]} }, { MODKEY|ControlMask,           XK_space,  setlayout,      {0} }, { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
