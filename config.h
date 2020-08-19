366e2cdb220ad91f48b1e04c5b88fb334dc315b5
aa-83 <elhegre@protonmail.com>
2020-08-19 16:36:07 +0200

    Fix on dmenu_cmd colours

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", /*normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor,*/NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x26", NULL };
@@ -105,7 +105,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[5]} }, { MODKEY|ControlMask,           XK_space,  setlayout,      {0} }, { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ControlMask,           XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
