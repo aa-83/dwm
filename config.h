e1c5dda46f1e74121a7010db3b0c6f1d20774762
aa-83 <elhegre@protonmail.com>
2020-09-01 14:23:58 +0200

    Updates to fonts

--- config.h
+++ config.h
@@ -11,8 +11,15 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true", "adobe-source-han-serif-otc-fonts=14:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = {
+	"monospace:pixelsize=11:antialias=true:autohint=true",
+	"Inconsolata Nerd Font Mono:size=11:style=regular:antialias=true:autohint=true",
+	"Inconsolata For Powerline:size=11:style=medium:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=9:style=regular:antialias=true:autohint=true",
+	"Source Han Serif:pixelsize=11:style=regular:antialias=true:autohint=true"
+};
+
+static const char dmenufont[]       = "monospace:size=12:antialias=true:autohint=true";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
